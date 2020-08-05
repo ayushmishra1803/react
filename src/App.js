8791225b4dd035479badfecca86d54aa4f20e6f0
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-05 22:03:32 +0530

    Phush

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Person from "./Person/Person"
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React Please Be Nice  To Me </h1>
+        <Person></Person>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+import React from "react"
+const person = () => {
+    return <p> Meko Nind AA rhi hai </p>
+}
+export default person
