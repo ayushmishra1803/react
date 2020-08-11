ffcd89eb7b9366a607930e7f0014253795e7b661
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-12 01:04:45 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Person from "./Person/Person"
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [{ name: "Ayush", age: 18 },
+    { name: "Mishra", age: 20 },
+    ]
+  }
   render() {
+
     return (
       <div className="App">
         <h1>Hello React Please Be Nice  To Me </h1>
-        <Person name="Ayush" age="20"><div><form><input type="readonly" name="ayush" value="Aysuh" /></form></div> </Person>
+        <Person name={this.state.persons[0].name}><div><form><input type="readonly" name={this.state.persons[0].name} value="Aysuh" /></form></div> </Person>
         <Person name="Chotu" >My age is 20  </Person>
         <Person name="Kuchbi" >My age is 20  </Person >
       </div>
