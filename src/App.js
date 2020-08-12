abcb52642ad0f379d25f27e8944795c276909140
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-12 22:30:34 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ class App extends Component {
     { name: "Mishra", age: 20 },
     ]
   }
+  onClickHandler() {
+    // console.log('Was Clicked');
+    this.setState({
+      persons: [{
+        name: "Ayush Mishra", age: 30
+      }, {
+        name: "XYZ", age: 32
+      }]
+    })
+  }
   render() {
 
     return (
       <div className="App">
         <h1>Hello React Please Be Nice  To Me </h1>
-        <Person name={this.state.persons[0].name}><div><form><input type="readonly" name={this.state.persons[0].name} value="Aysuh" /></form></div> </Person>
-        <Person name="Chotu" >My age is 20  </Person>
-        <Person name="Kuchbi" >My age is 20  </Person >
+        <button onClick={this.onClickHandler}>OnClick</button>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >My age is 20  </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My age is 20  </Person >
       </div>
     );
   }
