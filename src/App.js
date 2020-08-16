529e28c1dd19d1e36a2cd2c6dcd5f3b44c1816a5
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-17 01:11:35 +0530

    UPdates

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from "./Person/Person"
 import './App.css';
 
-class App extends Component {
-  state = {
-    persons: [{ name: "Ayush", age: 18 },
-    { name: "Mishra", age: 20 },
-    ]
-  }
-  onClickHandler = () => {
+const app = (props) => {
+  const [personState, setStateperson] = useState({
+    person: [{
+      name: "Ayush", age: 20
+    }, {
+      name: "Mishra", age: 50
+    }]
+  })
+  const clickHandler = () => {
+    setStateperson({
+      person: [{
+        name: "Changed Name", age: 200
 
-    this.setState({
-      persons: [{ name: "Ayush Mishra", age: 20 },
-      { name: "XYZ", age: 20 },
-      ]
+      }, {
+        name: "Changed", age: 1212
+      }]
     })
   }
-  render() {
-
-    return (
-      <div className="App">
-        <h1>Hello React Please Be Nice  To Me </h1>
-        <button onClick={this.onClickHandler}>OnClick</button>
-
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >  </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >  </Person >
-      </div>
-    );
-  }
+  return (
+    <div className="app">
+      <h1 >Hello React UseSate Hooks</h1>
+      <button onClick={clickHandler}> Submit</button>
+      <Person name={personState.person[0].name} age={personState.person[0].age} />
+      <Person name={personState.person[1].name} age={personState.person[1].age} />
+    </div>
+  )
 }
 
-export default App;
+export default app;
