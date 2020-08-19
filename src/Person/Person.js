4c5fa49afd201059c0fba81f9c78b5127a34034e
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-19 15:44:21 +0530

    UPdates

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Person.css"
 const person = (props) => {
 	return (
-		<div>
+		<div className="Hello">
 			<p onClick={props.click}>
 				Hello My name is {props.name} My Age is {props.age}
 			</p>
