180568ac22df2c2e09dd547621a89b7b5f998095
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-24 00:25:04 +0530

    U[pdates

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,31 +1,29 @@
 import React from "react";
 import "./Person.css";
-import Radium from "radium"
-import Styled from "styled-components"
+import Radium from "radium";
+import Styled from "styled-components";
 import styled from "styled-components";
-const person = (props) => {
-	const Styled = styled.div`
-		width: 60%;
-		margin: auto;
-		border: 0px #eee;
-		box-shadow: 0px 2px 3px #ccc;
-		text-align: center;
-		padding: 16px;
 
-		"@media(min-width:500px)": {
-			width: "450px";
-		}
-	`;
-	
+const StyledDiv = styled.div`
+	width: 60%;
+	margin: auto;
+	border: 0px #eee;
+	box-shadow: 0px 2px 3px #ccc;
+	text-align: center;
+	padding: 16px;
+
+	@media (min-width: 500px)  {
+		width: 450px;
+	}
+`;
+const person = (props) => {
 	return (
-		<Styled>
-		<div >
-			<p >
+		<StyledDiv>
+			<p>
 				Hello My name is {props.name} My Age is {props.age}
 			</p>
 			<input type="text" onChange={props.changed} value={props.name} />
-		</div>
-		</Styled>
+		</StyledDiv>
 	);
 };
-export default  person;
+export default person;
